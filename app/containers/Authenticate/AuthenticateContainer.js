f9a4deccdcc998b43786f19fbe36e0082314ec36
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2017-05-13 17:19:04 -0400

    Complete Async Actions With Redux Thunk

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,5 +1,4 @@
 import React, { PropTypes, Component } from 'react'
-import auth from 'helpers/auth'
 import { Authenticate } from 'components'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -7,14 +6,7 @@ import * as userActionCreators from 'redux/modules/users'
 
 class AuthenticateContainer extends Component {
   handleAuth = () => {
-    this.props.fetchingUser()
-
-    auth().then((user) => {
-      this.props.fetchingUserSuccess(user.uid, user, Date.now())
-      this.props.authUser(user.uid)
-    }).catch((error) => {
-      this.props.fetchingUserFailure(error.msg)
-    })
+    this.props.fetchAndHandleAuthedUser()
   }
 
   render() {
@@ -31,10 +23,7 @@ class AuthenticateContainer extends Component {
 AuthenticateContainer.propTypes = {
   isFetching: PropTypes.bool.isRequired,
   error: PropTypes.string.isRequired,
-  authUser: PropTypes.func.isRequired,
-  fetchingUser: PropTypes.func.isRequired,
-  fetchingUserSuccess: PropTypes.func.isRequired,
-  fetchingUserFailure: PropTypes.func.isRequired,
+  fetchAndHandleAuthedUser: PropTypes.func.isRequired,
 }
 
 function mapStateToProps (state) {
--- app/redux/modules/users.js
+++ app/redux/modules/users.js
@@ -1,3 +1,5 @@
+import auth from 'helpers/auth'
+
 // ACTION CONSTANTS
 const AUTH_USER = 'AUTH_USER'
 const UNAUTH_USER = 'UNAUTH_USER'
@@ -6,38 +8,51 @@ const FETCHING_USER_FAILURE = 'FETCHING_USER_FAILURE'
 const FETCHING_USER_SUCCESS = 'FETCHING_USER_SUCCESS'
 
 // ACTION CREATORS
-export function authUser (uid) {
+function authUser (uid) {
   return {
     type: AUTH_USER,
     uid,
   }
 }
 
-export function unauthUser () {
+function unauthUser () {
   return {
     type: UNAUTH_USER,
   }
 }
 
-export function fetchingUser () {
+function fetchingUser () {
   return {
     type: FETCHING_USER,
   }
 }
 
-export function fetchingUserFailure (error) {
+function fetchingUserFailure (error) {
   return {
     type: FETCHING_USER_FAILURE,
     error,
   }
 }
 
-export function fetchingUserSuccess (uid, user, timestamp) {
+function fetchingUserSuccess (uid, user, timestamp) {
   return {
     type: FETCHING_USER_SUCCESS,
     uid,
     user,
     timestamp,
+  }
+}
+
+export function fetchAndHandleAuthedUser () {
+  return function (dispatch) {
+    dispatch(fetchingUser())
+
+    auth().then((user) => {
+      dispatch(fetchingUserSuccess(user.uid, user, Date.now()))
+      dispatch(authUser(user.uid))
+    }).catch((error) => {
+      dispatch(fetchingUserFailure(error.msg))
+    })
   }
 }
 
